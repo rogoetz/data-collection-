5696751df7d2a64229225ce8d27dbf7278c25404
rogoetz <rogoetz@ad.unc.edu>
2017-07-25 17:11:51 -0400

    messed around with database thingy

--- rogoetz-write-to-database.sh
+++ rogoetz-write-to-database.sh
@@ -9,7 +9,7 @@ MYDATABASE=favbooks
 MYTABLE=favbooks
 
 #put data where MySQL can find it 
-sudo cp ./tmp.csv /var/lib/mysql-files/
+sudo cp tmp.csv /var/lib/mysql-files/
 
 #enter data input 
 echo "Writing new data to table."
@@ -17,5 +17,5 @@ mysql -u"$MYSQLUSER" -p"$MYSQLPASS" -e "LOAD DATA INFILE '/var/lib/mysql-files/t
 echo "Congratulations, your data has been entered successfully."
 
 #put database into a .sql file 
-echo "Survey data dumped to file `date --iso-8601`-$MYDATABASE.sql"
-mysqldump -u"$MYSQLUSER" -p"$MYSQLPASS" $MYDATABASE > `date --iso-8601`-$MYDATABASE.sql
+echo "Survey data dumped to file `date --iso-8601=seconds`-$MYDATABASE.sql"
+mysqldump -u"$MYSQLUSER" -p"$MYSQLPASS" $MYDATABASE > `date --iso-8601=seconds`-$MYDATABASE.sql
